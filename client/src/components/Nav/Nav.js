e6aba7fb8466fb77580d3c81e9a000ec897d6509
ektravel <38017980+ektravel@users.noreply.github.com>
2018-07-09 17:26:23 -0500

    removed materialize, added bootstrap, updated Nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,17 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Nav = () => (
-<nav>
-<div className="nav-wrapper">
-  <Link className="brand-logo" to="/">Friend Bubble</Link>
-  <ul id="nav-mobile" className="right hide-on-med-and-down">
-    <li><Link className="nav-link" to="/">home</Link></li>
-    <li><Link className="nav-link" to="/about">about</Link></li>
-    <li><Link className="nav-link" to="/login">login</Link></li>
-    <li><Link className="nav-link" to="/contact">contact</Link></li>
-  </ul>
-</div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <Link className="navbar-brand" to="/">Friend Bubble</Link>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+      <Link className="nav-link" to="/">home</Link>
+      </li>
+      <li className="nav-item">
+      <Link className="nav-link" to="/about">about</Link>
+      </li>
+      <li className="nav-item">
+      <Link className="nav-link" to="/login">login</Link>
+      </li>
+      <li className="nav-item">
+      <Link className="nav-link" to="/contact">contact</Link>
+      </li>
+    </ul>
+  </div>
 </nav>
+
 );
   
 export default Nav;
