c79b02d63b8310aaefd752a3ea7284a5b9278f22
ektravel <38017980+ektravel@users.noreply.github.com>
2018-07-22 23:14:26 -0500

    fixed parks DB bug

--- client/src/pages/Plans/Plans.js
+++ client/src/pages/Plans/Plans.js
@@ -23,8 +23,9 @@ class Plans extends Component {
     event.preventDefault();
   };
 
-  postPlan = event => {
+  postPlans = event => {
     event.preventDefault();
+    //alert('test');
     const { title, guests, activity, relationship, date, notes} = this.state;
     axios.post("/api/plans", { title, guests, activity, relationship, date, notes }).then(res => {
       console.log(res);
